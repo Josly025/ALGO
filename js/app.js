f434f8c1448a7b4e67994f8928bf78fb131797f2
Grantjos12 <grantjoslyn@gmail.com>
2021-10-12 12:15:01 -0500

    review of object Class

---
+++ js/app.js
@@ -0,0 +1,14 @@
+//ALGO PRACTICE
+
+//CLASS review --- You use classes to make objects
+// use the word class to create a class - use the method constructor to define the values
+class DogName {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+}
+
+let dog1 = new DogName("Jeff", 1);
+let dog2 = new DogName("George", 10);
+let dog3 = new DogName("Jorge", 23);
