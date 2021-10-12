f6ac4c87785e1039b32e815168d9abf9d5ff20fd
Grantjos12 <grantjoslyn@gmail.com>
2021-10-12 14:21:09 -0500

    first cardio session complete

--- js/app.js
+++ js/app.js
@@ -104,7 +104,33 @@ reverseInt(123); ///321
 // ex. capitalizeLetters("i love javascript") === "I Love Javascript"
 
 function capitalizeLetters(str) {
-  return caps;
+  const strArr = str.toLowerCase().split(" "); //maker sure this is spaced
+  //looping over array and objects use for in
+  for (i in strArr) {
+    strArr[i] =
+      strArr[i].substring(0, 1).toUpperCase() + strArr[i].substring(1);
+  }
+  let caps = strArr.join(" ");
+  console.log(console.log(caps));
 }
 
 capitalizeLetters("i love javascript");
+
+//Challenge 5: Fizzbuzz
+// Write a program that prints all the numbers from 1 to 100.
+// For multiples of 3, instead of the number, print "Fizz", for multiples of 5 print "Buzz".
+// For numbers which are multiples of both 3 and 5, print ("Fizzbuzz")
+
+function fizzBuzz() {
+  for (let i = 1; i < 101; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log("FizzBuzz");
+    } else if (i % 3 === 0) {
+      console.log("Fizz");
+    } else if (i % 5 === 0) {
+      console.log("Buzz");
+    } else console.log(i);
+  }
+}
+
+fizzBuzz();
