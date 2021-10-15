44a3cc3382e5edeab039fba210f0b5f6056d56c6
Grantjos12 <grantjoslyn@gmail.com>
2021-10-14 22:23:26 -0500

    added app2 for cardio v2

---
+++ js/app2.js
@@ -0,0 +1,15 @@
+///JavaScript Cardio version two
+
+// 1. Longest Word
+// Return to longest word in a string
+// If more than one longest word, put into an array
+// ex. longestWord("Hello, my name is Grant") ===> return Hello and Grant // 5 characters each
+// ex. longestWord("Hello, my name is guy") ===> return guy
+
+// 3. Flatten Array
+// 4. Anagrams
+// 5. Letter Changes
+
+function longestWord() {}
+
+longestWord("what is the longest word");
