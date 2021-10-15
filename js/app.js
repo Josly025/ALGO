2945945ce542b7b79fa48c5fd108e0a8cb93c471
Grantjos12 <grantjoslyn@gmail.com>
2021-10-14 20:14:50 -0500

    update

--- js/app.js
+++ js/app.js
@@ -125,11 +125,15 @@ function fizzBuzz() {
   for (let i = 1; i < 101; i++) {
     if (i % 3 === 0 && i % 5 === 0) {
       console.log("FizzBuzz");
+      return "FizzBuzz";
     } else if (i % 3 === 0) {
       console.log("Fizz");
+      return "Fizz";
     } else if (i % 5 === 0) {
       console.log("Buzz");
+      return "Buzz";
     } else console.log(i);
+    return i;
   }
 }
 
