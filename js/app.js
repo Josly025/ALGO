79fce34d9cea6879f02f6866232693450774fc15
Grantjos12 <grantjoslyn@gmail.com>
2021-10-18 22:11:18 -0500

    solve and add primes

--- js/app.js
+++ js/app.js
@@ -152,3 +152,62 @@ function fizzBuzz() {
 }
 
 fizzBuzz();
+
+//CHALLENGE 5: MAX CHARACTER
+// Return the character that is the most common in a string
+// ex. maxCharacter("javascript") == "a"
+
+function maxCharacter(str) {
+  //character map
+  let charMap = {};
+  let maxNum = 0;
+  let maxChar = "";
+
+  for (let char of str) {
+    if (charMap[char]) {
+      charMap[char]++;
+    } else {
+      charMar[char] = 1;
+    }
+  }
+
+  ///loop through charMap
+  for (let char in charMap) {
+    if (charMap[char] > maxNum) {
+      maxNum = charMap[char];
+      maxChar = char;
+    }
+  }
+  return maxChar;
+  console.log(maxChar);
+}
+
+maxCharacter("javascript"); //return a
+
+
+
+
+
+function maxCharacter(str){
+let charMap = {};
+let maxNum = 0;
+let maxChar = ""
+
+for(let char of str){
+  if(charMap[char]){
+    charMap[char] = ++;
+  }else{
+    charMap[char] = 1;
+  }
+}
+
+for(let char in charMap){
+  if(charMap[char] >  charMax){
+    maxNum = charMap[char];
+    maxChar = char;
+  }
+}
+
+
+return maxChar 
+}
--- js/app2.js
+++ js/app2.js
@@ -6,10 +6,63 @@
 // ex. longestWord("Hello, my name is Grant") ===> return Hello and Grant // 5 characters each
 // ex. longestWord("Hello, my name is guy") ===> return guy
 
+function longestWord() {}
+
+longestWord("what is the longest word");
+
 // 3. Flatten Array
 // 4. Anagrams
 // 5. Letter Changes
 
-function longestWord() {}
+// function maxChars(str) {
+//   //charMap //create an object
+//   let charMap = {};
+//   //numMax
+//   let maxNum = 0;
+//   //maxChar
+//   let maxChar = "";
 
-longestWord("what is the longest word");
+//   for (let char of str) {
+//     if (charMap[char]) {
+//       charMap[char]++;
+//     } else {
+//       charMap[char] = 1;
+//     }
+//   }
+
+//   for (let char in charMap) {
+//     if (charMap[char] > maxNum) {
+//       maxNum = charMap[char];
+//       maxChar = char;
+//     }
+//   }
+//   console.log(maxChar);
+//   return maxChar;
+// }
+
+// maxChars("javascript");
+
+/// CHALLENGE 1: ADD ALL NUMBERS
+// Return a sum of all parameters entered regardless of the amount of numbers - No awways
+// ex. addALL(2,5,6,7) ===> 20
+
+function addAll(...numbers) {
+  let total = 0;
+
+  numbers.forEach((number) => {
+    console.log(number);
+    total += number;
+  });
+  console.log(total);
+  return total;
+}
+
+addAll(2, 5, 6, 7, 5);
+
+///Challenge 2: SUM ALL PRIMES
+// Pass in a number to loop up to and add all of the prime numbers. A prime number is a whole number greater than 1 who only factors are 1 and itself
+// ex. sumAllPrimes(10) === 17
+
+function sumAllPrimes(num) {}
+
+console.log(sumAllPrimes(10));
